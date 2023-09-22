7de450e5111de348cd9e99c7dad8018215aa8ce3
dg-Tech1 <130319167+dg-Tech1@users.noreply.github.com>
2023-09-22 20:15:58 +0200

    #Ancre index.js
    #Ancre 
    1. -commencer par ajouter l'ancre au menu
    const Menu = () => {
      // Fonction pour définir l'ancre
      const setHash = (hash) => {
        window.document.location.hash = hash;
      };
       
      -ensuite ajouter les id;"" correspondant dans le page/home

--- src/containers/Menu/index.js
+++ src/containers/Menu/index.js
@@ -4,24 +4,37 @@ import Logo from "../../components/Logo";
 
 import "./style.scss";
 
-const Menu = () => (
-  <nav>
-    <Logo />
-    <ul>
-      <li>
-        <a href="#nos-services">Nos services</a>
-      </li>
-      <li>
-        <a href="#nos-realisations">Nos réalisations</a>
-      </li>
-      <li>
-        <a href="#notre-equipe">Notre équipe</a>
-      </li>
-    </ul>
-    <Button title="contact" onClick={() => (window.document.location.hash = "#contact")}>
-      Contact
-    </Button>
-  </nav>
-);
+const Menu = () => {
+  // Fonction pour définir l'ancre
+  const setHash = (hash) => {
+    window.document.location.hash = hash;
+  };
+
+  return (
+    <nav>
+      <Logo />
+      <ul>
+        <li>
+          <a href="#nos-services" onClick={() => setHash("#nos-services")}>
+            Nos services
+          </a>
+        </li>
+        <li>
+          <a href="#nos-realisations" onClick={() => setHash("#nos-realisations")}>
+            Nos réalisations
+          </a>
+        </li>
+        <li>
+          <a href="#notre-equipe" onClick={() => setHash("#notre-equipe")}>
+            Notre équipe
+          </a>
+        </li>
+      </ul>
+      <Button title="contact" onClick={() => setHash("#contact")}>
+        Contact
+      </Button>
+    </nav>
+  );
+};
 
 export default Menu;
