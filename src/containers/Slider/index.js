94e895319a72c7df38205f5aef8b36d2a37aa5c4
dg-Tech1 <130319167+dg-Tech1@users.noreply.github.com>
2023-09-23 00:34:41 +0200

    ModifCarroussel index.js
    Affichage date par ordre croissant

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -5,55 +5,61 @@ import { getMonth } from "../../helpers/Date";
 import "./style.scss";
 
 const Slider = () => {
-  const { data } = useData();
-  const [index, setIndex] = useState(0);
+  const { data } = useData(); // Récupérer les données du contexte DataContext
+  const [index, setIndex] = useState(0); // Initialiser un état local 'index' à 0
+
+  // Trier les événements par date en ordre décroissant
   const byDateDesc = data?.focus.sort((evtA, evtB) =>
-    new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
+    new Date(evtA.date) < new Date(evtB.date) ? 1 : -1
   );
-  const nextCard = () => {
+
+  const imgSlide = data?.focus?.length; // Obtenir le nombre total d'images de diapositive
+
+  const nextCard = () => { // Définir une fonction nextCard
     setTimeout(
-      () => setIndex(index < byDateDesc.length ? index + 1 : 0),
+      () => setIndex(index < imgSlide - 1 ? index + 1 : 0), // Passer à la diapositive suivante (ou revenir à la première) toutes les 5 secondes
       5000
     );
   };
-  useEffect(() => {
+
+  useEffect(() => { // Appeler nextCard lorsque le composant est monté
     nextCard();
   });
+
   return (
-    <div className="SlideCardList">
+    <div className="SlideCardList"> {/* Élément contenant le carrousel */}
+      {/* Mapping des diapositives des événements */}
       {byDateDesc?.map((event, idx) => (
-        <>
-          <div
-            key={event.title}
-            className={`SlideCard SlideCard--${
-              index === idx ? "display" : "hide"
-            }`}
-          >
-            <img src={event.cover} alt="forum" />
-            <div className="SlideCard__descriptionContainer">
-              <div className="SlideCard__description">
-                <h3>{event.title}</h3>
-                <p>{event.description}</p>
-                <div>{getMonth(new Date(event.date))}</div>
-              </div>
+        <div
+          key={event.title} // Clé unique basée sur le titre de l'événement
+          className={`SlideCard SlideCard--${index === idx ? "display" : "hide"}`} // Appliquer la classe 'display' si l'index correspond à la diapositive actuelle, sinon 'hide'
+        >
+          <img src={event.cover} alt="forum" /> {/* Afficher l'image de l'événement */}
+          <div className="SlideCard__descriptionContainer">
+            <div className="SlideCard__description">
+              <h3>{event.title}</h3> {/* Afficher le titre de l'événement */}
+              <p>{event.description}</p> {/* Afficher la description de l'événement */}
+              <div>{getMonth(new Date(event.date))}</div> {/* Afficher le mois de la date de l'événement en utilisant la fonction getMonth */}
             </div>
           </div>
-          <div className="SlideCard__paginationContainer">
-            <div className="SlideCard__pagination">
-              {byDateDesc.map((_, radioIdx) => (
-                <input
-                  key={`${event.id}`}
-                  type="radio"
-                  name="radio-button"
-                  checked={idx === radioIdx}
-                />
-              ))}
-            </div>
-          </div>
-        </>
+        </div>
       ))}
+
+      <div className="SlideCard__paginationContainer">
+        <div className="SlideCard__pagination">
+          {byDateDesc?.map((focus, radioIdx) => (
+            <input
+              key={focus.title} // Clé unique basée sur le titre de l'événement
+              type="radio"
+              name="radio-button"
+              checked={index === radioIdx} // Cocher le bouton radio si l'index correspond à la diapositive actuelle
+              readOnly
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
 
-export default Slider;
+export default Slider; // Exporter le composant Slider
