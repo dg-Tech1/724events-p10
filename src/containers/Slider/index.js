445316f6b91744659f06165f1aae6f7236f8b88f
dg-Tech1 <130319167+dg-Tech1@users.noreply.github.com>
2023-09-24 21:08:57 +0200

    caroussel slider index.js
    le carroussel sliders pour un ordre décroissant modifier container/slider et pages/home

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -5,61 +5,55 @@ import { getMonth } from "../../helpers/Date";
 import "./style.scss";
 
 const Slider = () => {
-  const { data } = useData(); // Récupérer les données du contexte DataContext
-  const [index, setIndex] = useState(0); // Initialiser un état local 'index' à 0
-
-  // Trier les événements par date en ordre décroissant
+  const { data } = useData();
+  const [index, setIndex] = useState(0);
   const byDateDesc = data?.focus.sort((evtA, evtB) =>
     new Date(evtA.date) < new Date(evtB.date) ? 1 : -1
   );
-
-  const imgSlide = data?.focus?.length; // Obtenir le nombre total d'images de diapositive
-
-  const nextCard = () => { // Définir une fonction nextCard
+  const slides = data?.focus.length;
+  const nextCard = () => {
     setTimeout(
-      () => setIndex(index < imgSlide - 1 ? index + 1 : 0), // Passer à la diapositive suivante (ou revenir à la première) toutes les 5 secondes
+      () => setIndex(index < slides-1 ? index + 1 : 0),
       5000
     );
   };
-
-  useEffect(() => { // Appeler nextCard lorsque le composant est monté
+  useEffect(() => {
     nextCard();
   });
-
   return (
-    <div className="SlideCardList"> {/* Élément contenant le carrousel */}
-      {/* Mapping des diapositives des événements */}
+    <div className="SlideCardList">
       {byDateDesc?.map((event, idx) => (
-        <div
-          key={event.title} // Clé unique basée sur le titre de l'événement
-          className={`SlideCard SlideCard--${index === idx ? "display" : "hide"}`} // Appliquer la classe 'display' si l'index correspond à la diapositive actuelle, sinon 'hide'
-        >
-          <img src={event.cover} alt="forum" /> {/* Afficher l'image de l'événement */}
-          <div className="SlideCard__descriptionContainer">
-            <div className="SlideCard__description">
-              <h3>{event.title}</h3> {/* Afficher le titre de l'événement */}
-              <p>{event.description}</p> {/* Afficher la description de l'événement */}
-              <div>{getMonth(new Date(event.date))}</div> {/* Afficher le mois de la date de l'événement en utilisant la fonction getMonth */}
+          <div
+            key={event.title}
+            className={`SlideCard SlideCard--${
+              index === idx ? "display" : "hide"
+            }`}
+          >
+            <img src={event.cover} alt="forum" />
+            <div className="SlideCard__descriptionContainer">
+              <div className="SlideCard__description">
+                <h3>{event.title}</h3>
+                <p>{event.description}</p>
+                <div>{getMonth(new Date(event.date))}</div>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
-
-      <div className="SlideCard__paginationContainer">
-        <div className="SlideCard__pagination">
-          {byDateDesc?.map((focus, radioIdx) => (
-            <input
-              key={focus.title} // Clé unique basée sur le titre de l'événement
-              type="radio"
-              name="radio-button"
-              checked={index === radioIdx} // Cocher le bouton radio si l'index correspond à la diapositive actuelle
-              readOnly
-            />
-          ))}
-        </div>
-      </div>
+        ))}
+          <div className="SlideCard__paginationContainer">
+            <div className="SlideCard__pagination">
+              {byDateDesc?.map((event, radioIdx) => (
+                <input
+                  key={event.title}
+                  type="radio"
+                  name="radio-button"
+                  checked={index === radioIdx}
+                  readOnly
+                />
+              ))}
+            </div>
+          </div>
     </div>
   );
 };
 
-export default Slider; // Exporter le composant Slider
+export default Slider;
